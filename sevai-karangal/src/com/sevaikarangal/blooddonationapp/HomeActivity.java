522e21a6385602585b005ddc3fa8f33e316aa397
asbalasubramania <asbalasubramania@ebay.com>
2014-11-08 15:29:45 +0530

    New Activities

--- sevai-karangal/src/com/sevaikarangal/blooddonationapp/HomeActivity.java
+++ sevai-karangal/src/com/sevaikarangal/blooddonationapp/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.sevaikarangal.blooddonationapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -11,11 +13,19 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+    
+    public void openRequestActivity(View view) {
+    	Intent intent = new Intent(this, RequestActivity.class);
+    	startActivity(intent);
+    }
 
-
+    public void openDonateActivity(View view) {
+    	Intent intent = new Intent(this, SubscribeActivity.class);
+    	startActivity(intent);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
