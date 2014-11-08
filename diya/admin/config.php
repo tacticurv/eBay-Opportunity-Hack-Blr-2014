14709c256e3e3b114a577e910f454228c422ccda
tacticurv <tacticurv@gmail.com>
2014-11-08 22:45:00 +0530

    First Commit

---
+++ diya/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/diya/admin/');
+define('HTTP_CATALOG', 'http://localhost/diya/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/diya/admin/');
+define('HTTPS_CATALOG', 'http://localhost/diya/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\diya/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\diya/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\diya/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\diya/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\diya/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\diya/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\diya/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\diya/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\diya/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\diya/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\diya/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\diya/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'diya');
+define('DB_PREFIX', 'oc_');
