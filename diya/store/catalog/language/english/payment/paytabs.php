80c9de390b31037eae71b0597a361e0e73693c03
tacticurv <tacticurv@gmail.com>
2014-11-09 16:04:15 +0530

    Diya Final Commit

---
+++ diya/store/catalog/language/english/payment/paytabs.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+    // Text
+   $_['text_title'] = 'PayTabs Payment Gateway';
+
+
+  $_['heading_title']     = 'Thank you for shopping with %s .... ';
+
+// Text
+$_['text_title']        = ' PayTabs Payment Gateway';
+$_['text_response']     = 'Response from PayTabs:';
+$_['text_success']      = '... your payment was successfully received.';
+$_['text_success_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b> whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
+?>
