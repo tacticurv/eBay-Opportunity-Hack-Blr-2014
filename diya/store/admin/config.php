a922083717e0e6b73596a76f2e33665e893209b5
tacticurv <tacticurv@gmail.com>
2014-11-09 10:35:45 +0530

    Diya almost done

---
+++ diya/store/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/diya/store/admin/');
+define('HTTP_CATALOG', 'http://localhost/diya/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/diya/store/admin/');
+define('HTTPS_CATALOG', 'http://localhost/diya/store/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\diya\store/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\diya\store/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\diya\store/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\diya\store/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\diya\store/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\diya\store/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\diya\store/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\diya\store/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\diya\store/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\diya\store/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\diya\store/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\diya\store/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'diya');
+define('DB_PREFIX', 'oc_');
---
+++ diya/store/catalog/language/english/checkout/error.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Oops..!';
+
+// Text
+$_['text_customer'] = '<p> The Payment is rejected.<p>';
+$_['text_checkout'] = 'Checkout';
+$_['text_success']  = '<p> The Payment is rejected.<p>';
+$_['text_guest']    = '<p> The Payment is rejected.<p>';
+?>
